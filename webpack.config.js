2935c006041b4e501acb30b9fdc7863b3517c033
Diana Butiakova <butiakova.diana@gmail.com>
2025-03-25 01:12:04 +0600

    fix: set devtool=false in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
 const target = devMode ? 'web' : 'browserslist';
-const devtool = devMode ? 'source-map' : undefined;
+const devtool = devMode ? 'source-map' : false;
 
 module.exports = {
   mode,
